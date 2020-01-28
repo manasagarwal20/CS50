f007e851662b479abb5d496c1dae9f3a11ce7347
manasagarwal20 <manasagarwal20@users.noreply.github.com>
2020-01-28 13:11:32 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- cash.c
+++ cash.c
@@ -5,32 +5,36 @@
 int main(void)
 {
     float input = get_float("Change owed: ");
-    while(input<0)
+    while (input < 0)
     {
         input = get_float("Change owed: ");
     }
     int coins = round(input * 100);
 
-    int count=0;
-    while(coins>=25)
+    int count = 0;
+    while (coins >= 25)
     {
-        coins=coins-25;
+        // if still denomination with 25c change
+        coins = coins - 25;
         count++;
     }
-    while(coins>=10)
+    while (coins >= 10)
     {
-       coins=coins-10;
+        // if still denomination with 10c change
+        coins = coins - 10;
         count++;
     }
-    while(coins>=5)
+    while (coins >= 5)
     {
-       coins=coins-5;
+        // if still denomination with 5c change
+        coins = coins - 5; 
         count++;
     }
-    while(coins>=1)
+    while (coins >= 1)
     {
-       coins=coins-1;
+        // if still denomination with 1c change
+        coins = coins - 1;
         count++;
     }
-    printf("%d\n",count);
+    printf("%d\n", count);
 }
