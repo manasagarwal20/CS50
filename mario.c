c5574d983aef1d4aac4e06285ae285806ae43114
manasagarwal20 <manasagarwal20@users.noreply.github.com>
2020-01-28 09:55:55 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -3,23 +3,25 @@
 
 int main(void)
 {
-int n=get_int("Height: ");
+int n;
+n=get_int("Height: ");
 while(n>8 || n<1)
 {
     n=get_int("Height: ");
 }
 for(int i=0;i<n;i++)
 {
-for(int j=0;j<n;j++)
-{
-if((j+i+1)>=n)
-{
-    printf("#");
-}
-    else
+    for(int j=0;j<n;j++)
     {
-        printf(" ");
-    }
+        if((j+i+1)>=n)
+        {
+            printf("#");
+        }
+        else
+        {
+            printf(" ");
+        }
+     }
+printf("\n");
 }
-printf("\n");}
 }
