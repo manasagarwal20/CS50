a9790d15042c6a64a09e43117f6283738a99906a
manasagarwal20 <manasagarwal20@users.noreply.github.com>
2020-01-29 10:55:25 +0000

    automated commit by check50 [check50=True]

---
+++ caesar.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<cs50.h>
+#include<ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[] )
+{
+    if(argc==2)
+    if(atoi(argv[1]))
+    {
+        int test = atoi(argv[1]);
+       // int x = &test;
+       char c;
+        string str = get_string("Plaintext: ");
+        int n = strlen(str);
+        for(int i = 0; i < n; i++)
+        {
+            c = str[i];
+            c=(c+test);
+            if((c>=65 && c<=90) || (c>=97 && c<=122))
+            {
+                str[i]=c;
+            }
+            else
+            {
+                c = c - 26;
+                str[i] = c;
+            }
+
+        }
+        printf("Ciphertext: %s\n", str);
+    }
+
+    else
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+    else
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+}
