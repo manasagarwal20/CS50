3849e636580a3f0ae8833dae0a60fa377feb6778
manasagarwal20 <manasagarwal20@users.noreply.github.com>
2020-01-31 06:23:51 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -2,10 +2,9 @@ h = -1
 while h > 8 or h <= 0:
     h = input("Height: ")
     if h.isalpha() or h == '':
-        h=-1
+        h = -1
     else:
         h = int(h)
 
 for i in range(1, h + 1):
-
-        print(' ' * (h - i) + "#" * i)
+    print(' ' * (h - i) + "#" * i)
